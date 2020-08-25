11d3c34c23ca23820b25e20aae1b0bf36fba3180
Dr. T.J. Jankun-Kelly <tjk@acm.org>
2020-08-24 20:10:18 -0500

    Fixed vertical scale and application of aspect ratio

--- lines.py
+++ lines.py
@@ -1,13 +1,9 @@
 """lines.py
 
 Generates some sparklines or simple line chart as a Canvas2D plot
-(sparkline) or a Pillow image (line chart).
+(sparkline).
 """
 import math
-
-style = "color: rgb(44,160,44)"
-style = "color: #C33"
-style = "color: #900; font-weight: bold"
 
 def bank_data(data):
     """Using the median approach, bank to 45 degrees the data.  
@@ -26,11 +22,10 @@ def bank_data(data):
     if len(slopes) % 2 != 0:
         median_slope = slopes[len(slopes) // 2]
     else:
-        median_slope = 0.5 * sum(slopes[len(slopes) // 2], slopes[len(slopes) // 2 - 1])
+        median_slope = 0.5 * sum([slopes[len(slopes) // 2], slopes[len(slopes) // 2 - 1]])
     return median_slope * (max_x - min_x) / (max_y - min_y)
 
 def generate_canvas(canvas_id, data, colors, height, width=None):
-    aspect = bank_data(data)
     min_x = min(x for (x,y) in data)
     min_y = min(y for (x,y) in data)
     max_x = max(x for (x,y) in data)
@@ -38,18 +33,13 @@ def generate_canvas(canvas_id, data, colors, height, width=None):
     h = height
     if max_y < 0.5:
         h = max_y * height / 0.5
-    x_mapper = lambda x: (x - min_x) / (max_x - min_x) * width + 0.5
-    y_mapper = lambda y: (max_y - y) / (max_y - min_y) * h + 0.5 + (height - h)
 
     if width is None:
         # Sparkline: Fit given line height
-        width = round(height * aspect)
-    else:
-        # Fit best into given image
-        if aspect <= 1:
-            height = round(width / aspect)
-        else:
-            width = round(height * aspect)
+        width = round(h * bank_data(data))
+
+    x_mapper = lambda x: (x - min_x) / (max_x - min_x) * width + 0.5
+    y_mapper = lambda y: (max_y - y) / (max_y - min_y) * h + 0.5 + (height - h)
 
     script = f"const canvas_{canvas_id} = document.getElementById('{canvas_id}');\n"
     script += f"const context_{canvas_id} = canvas_{canvas_id}.getContext('2d');\n"
@@ -65,6 +55,3 @@ def generate_canvas(canvas_id, data, colors, height, width=None):
     script += f"context_{canvas_id}.stroke();\n"
 
     return f'<canvas id="{canvas_id}" height={height} width={width}></canvas><script>{script}</script>'
-
-def generate_image(data, colors, width, height):
-    pass
